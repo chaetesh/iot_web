311401a47fd2e5d7e9bfbbf246d582429cf77776
chaetesh <chaetesh@gmail.com>
2022-11-19 18:14:07 +0530

    navbar

---
+++ script.js
@@ -0,0 +1,19 @@
+// setTimeout(() => {
+//     //     // TO check media Querie is applied
+//     //     if (window.matchMedia("(max-width: 812px)").matches) {
+//     //         // Viewport is less or equal to 700 pixels wide
+//     //         const mainNav = document.getElementById("collapsable-nav");
+//     //         // const submenu = document.querySelectorAll(".nav-list")
+//     //         // const menuIcon = document.getElementById("menu-icon");
+//     //         document.getElementById("li2").style = "margin-top: 30px;";
+//     //         document.getElementById("li3").style = "margin-top: 30px;";
+        
+//             document.getElementById("navbar-toggler").addEventListener("click",function () {
+//                 const mainNav = document.getElementById("navbarSupportedContent");
+//                 if (mainNav.style.display == "block") {
+//                     console.log("here")
+//                     document.getElementById("toggler-icon").innerHTML="X";
+//                   }
+//             })
+        
+// }, 0);
